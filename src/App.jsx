6aef39775973f4e360cd7705f683f993640080a8
Alexander Scarlett <ascarlett92@yahoo.com>
2024-06-10 19:36:01 -0400

    installed bootstrap sass

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Spinner from './components/spinner'
 import NavBar from './components/navbar'
+import '../public/scss/bootstrap.scss'
 
 function App() {
   const [count, setCount] = useState(0)
