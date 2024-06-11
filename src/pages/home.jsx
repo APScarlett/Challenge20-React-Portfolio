2978e0432ba32c5a985dca28dfe7ea0745114edc
Alexander Scarlett <ascarlett92@yahoo.com>
2024-06-11 19:25:12 -0400

    updated css

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 function Home(){
     return <>
-        <h1>Home</h1>
+        <h1>Hello and welcome to my Page!</h1>
         <p class="text-dark pb-5 animated slideInDown">Thank you for taking the time to stop by my page. I am a new developer looking for oportunities to further my career.</p>
         <p class="text-dark pb-5 animated slideInDown">Please use the Navbar to take a look at what I have worked on so far.</p>
     </>
