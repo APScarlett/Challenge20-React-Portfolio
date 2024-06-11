3cf9365e4fa5b038410f245071d7d7356cf9e172
Alexander Scarlett <ascarlett92@yahoo.com>
2024-06-11 15:16:02 -0400

    added about,contact,home,portfolio,reusme.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,36 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Spinner from './components/spinner'
 import NavBar from './components/navbar'
-import './assets/scss/bootstrap.scss'
+import Home from './pages/home'
+import PortFolio from './pages/portfolio'
+import About from './pages/about'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <>
-      <Spinner/>
-      <NavBar/>
+\
+      <NavBar currentPage={currentPage} handlePageChange={handlePageChange}/>
+      {renderPage()}
     </>
   )
 }
---
+++ src/pages/portfolio.jsx
@@ -0,0 +1,13 @@
+import CardContainer from "../components/cardContainer"
+import Card from '../components/card'
+function PortFolio(){
+    return <>
+        <CardContainer>
+
+            <Card/>
+        </CardContainer>
+
+    </>
+}
+
+export default PortFolio
